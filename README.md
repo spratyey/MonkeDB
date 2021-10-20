e9bb12f73a277d8b3a04dd4376247e1edc5c03f1
Pratyay Suvarnapathaki <spratyay@gmail.com>
2021-10-21 02:08:15 +0530

    Vehicle Table, basic ops

---
+++ README.md
@@ -0,0 +1,29 @@
+## Hi,
+Here's how to set up docker and shit\
+The notion doc is soooorta wonky, but still, here: https://rvk7895.notion.site/rvk7895/Instructions-to-install-MySQL-in-Docker-b91ff88693544d639abcd30844a2ff86
+
+First, get this repo and cd into it.\
+Now, do everything from within here\
+**Step 1.** Get the docker engine :thumbs:\
+**Step 2.** use the `usermod` command to add yourself to the `docker` group. This is only to prevent spamming sudo for any docker command.\ 
+**Step 3.** Get MYSQL in docker: `docker pull ubuntu/mysql` (this works on arch too btw)\
+**Step 4.** Set up MYSQL environment within docker: `docker run --name=<SetAContainerName> -p 30306:3306 -e MYSQL_ROOT_PASSWORD=<SetAPassword> -d ubuntu/mysql`\
+
+Now, in no particular order of steps,
+- To start the docker-wala MySQL CLI,\
+`docker exec -it <ContainerName> mysql -uroot -p`
+
+- To construct a database within the docker MYSQL from the dump file, **ONLY the Vehicle table exists rn**\
+Copy the dump from this dir to docker's storage space: `docker cp ./monkeDB1.sql <ContainerName>:/monkeDB1.sql`\
+Then start the docker-wala MySQL CLI (same as above): `docker exec -it <ContainerName> mysql -uroot -p`\
+Then from within the MySQL CLI, `source monkeDB1.sql`
+
+- To run the program,\
+Ensure you have `PyMySQL`: `pip install PyMySQL`\
+`python3 MiniWorld.py`\
+**NOTE: Only Options 1 (insert vehicle) and 5 (Log Out) work rn. Just a basic working setup bruh.**
+
+- After you're done adding tables/doing anything remotely useful within the database other than screwing around,\
+i.e. if you have actually added meaningful tables to the database, to dump the database from within docker to a regular ol' file on your regular ol' real system\
+`docker exec -it <DockerSetName> mysqldump -uroot -p<SQLPassword> --databases <dbName> > <filename>.sql`\
+(Example: after creating the vehicle table, i did this so that now you guys can 'clone' the latest version of our db on your machines without having to manually create tables and stuff.)
---
+++ monkeDB1.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
+--
+-- Host: localhost    Database: monkeDB1
+-- ------------------------------------------------------
+-- Server version	8.0.26-0ubuntu0.21.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `monkeDB1`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `monkeDB1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `monkeDB1`;
+
+--
+-- Table structure for table `Vehicle`
+--
+
+DROP TABLE IF EXISTS `Vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Vehicle` (
+  `Lic_Number` varchar(11) NOT NULL,
+  `Vehicle_type` varchar(100) DEFAULT NULL,
+  `Hourly_Rate` int DEFAULT NULL,
+  PRIMARY KEY (`Lic_Number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Vehicle`
+--
+
+LOCK TABLES `Vehicle` WRITE;
+/*!40000 ALTER TABLE `Vehicle` DISABLE KEYS */;
+INSERT INTO `Vehicle` VALUES ('12345432121','Jeep',300),('MH04GY1232','Bus',500);
+/*!40000 ALTER TABLE `Vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-20 19:30:03
